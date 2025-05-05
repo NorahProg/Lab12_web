8efd028c34339bd851e74531f0176a51e806c994
NorahProg <Norah.imn1@gmail.com>
2025-05-05 14:59:24 +0300

    edited

--- apps/usermodule/urls.py
+++ apps/usermodule/urls.py
@@ -28,11 +28,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import register, CustomLoginView
-from django.contrib.auth.views import LogoutView
+from . import views
 
 
 urlpatterns = [
     path('register/',register, name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- apps/usermodule/views.py
+++ apps/usermodule/views.py
@@ -28,13 +28,10 @@
 
 
 from django.shortcuts import render, redirect
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-
 from django.contrib import messages
-
 from django.contrib.auth import logout
 
 # Create your views here.
@@ -52,10 +49,6 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'usermodule/register.html', {'form': form})
 
-# class CustomLoginView(LoginView):
-#     template_name = 'usermodule/login.html'
-#     next_page = reverse_lazy('list_students') 
-
 class CustomLoginView(LoginView):
     template_name = 'usermodule/login.html'
 
@@ -64,5 +57,5 @@ class CustomLoginView(LoginView):
 
 
 def logout_view(request):
-    logout(request)
+    logout(request)  
     return redirect('login')
